e757f9ddeeb71e2749a5dd1be1211e193e3527ea
Radosław <radek.kaczka1@gmail.com>
2022-12-15 11:11:36 +0100

    adding a cloud firestore, adding a form on  map to add a fishery

--- app/src/main/java/com/example/fisherhelper/MainActivity.kt
+++ app/src/main/java/com/example/fisherhelper/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.fisherhelper
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -13,9 +17,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         map_button.setOnClickListener {
           val intent = Intent(this,MapsActivity::class.java)
             startActivity(intent)
+
 
         }
     }
