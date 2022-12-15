700e25b625984465acf749b5f7d8faa7c061c8ea
Radosław <radek.kaczka1@gmail.com>
2022-12-15 11:04:52 +0100

    adding a cloud firestore, adding a form on  map to add a fishery

--- app/src/main/java/com/example/fisherhelper/MapsActivity.kt
+++ app/src/main/java/com/example/fisherhelper/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fisherhelper
 
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
@@ -13,13 +14,19 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import kotlinx.android.synthetic.main.activity_maps.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMapsSdkInitializedCallback {
 
 
     private lateinit var mMap: GoogleMap
+
     private lateinit var binding: ActivityMapsBinding
 
     private lateinit var currentLocation: Location
@@ -36,8 +43,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMapsSdkInitializ
 
 
 
+getCurrentLocationUser()
 
-        getCurrentLocationUser()
+getMarkersFromFireStore()
+    }
+
+    private fun getMarkersFromFireStore() {
+
+        val db = FirebaseFirestore.getInstance()
+        val ref = db.collection("coordinates")
+         ref.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                // document exists
+                val geopoint = document.getGeoPoint("geoPoint")
+                val name= document.get("name")
+                val latLng = LatLng(geopoint!!.latitude, geopoint!!.longitude)
+                mMap.addMarker(MarkerOptions().position(latLng).title(name as String?).icon(BitmapDescriptorFactory.fromResource(R.drawable.gear)))
+            }
+
+        }
+
     }
 
 
@@ -84,15 +110,74 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMapsSdkInitializ
 
 
     override fun onMapReady(googleMap: GoogleMap) {
-
+        mMap = googleMap
 //        val latLng = LatLng(currentLocation.latitude ,currentLocation.longitude)
-        val latLng = LatLng(50.029488 ,22.008091) //example location of rzeszow
+        val latLng = LatLng(50.029488, 22.008091) //example location of rzeszow
         val location = currentLocation.latitude.toString()
         val location1 = currentLocation.longitude.toString()
-        val markerOptions= MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title("Twoja lokalizacja $location $location1")
+        val markerOptions = MarkerOptions().position(latLng)
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            .title("Twoja lokalizacja $location $location1")
         googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
         googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
         googleMap?.addMarker(markerOptions)
+
+        val db = FirebaseFirestore.getInstance()
+        // pobranie referencji do kolekcji w Firestore
+        val collectionReference = db.collection("coordinates")
+
+
+// utworzenie obiektu GoogleMap za pomocą MapView
+
+        // wyświetlenie markera na mapie
+        val marker = googleMap.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Czy tutaj chcesz dodać łowisko?"))
+
+        // nasłuchiwanie kliknięć na mapie
+        mMap.setOnMapClickListener { latLng ->
+            // ustawienie pozycji markera na klikniętym miejscu na mapie
+            marker!!.position = latLng
+        }
+
+
+
+        add_marker_button.setOnClickListener {
+            // pobranie koordynatów markera
+            val latLng = marker!!.position
+
+            // pobranie nazwy markera z pola tekstowego
+            val markerName = marker_name_input.text.toString()
+
+            // utworzenie obiektu GeoPoint z koordynatami markera
+            val markerGeoPoint = GeoPoint(latLng.latitude, latLng.longitude)
+            collectionReference.add(
+                mapOf(
+                    "name" to markerName,
+                    "geoPoint" to markerGeoPoint
+                )
+            )
+                .addOnSuccessListener {
+                    // wyświetlenie komunikatu o powodzeniu dodawania markera
+                    getMarkersFromFireStore()
+                 val toast=   Toast.makeText(this, "Łowisko dodane pomyślnie", Toast.LENGTH_SHORT).show()
+//                    val view = toast.view
+//                    view!!.setBackgroundColor(Color.BLUE)
+//                    toast.show()
+                    marker.position = LatLng(0.0, 0.0)
+                    // wyczyszczenie pola tekstowego z nazwą markera
+                    marker_name_input.setText("")
+
+                }
+                .addOnFailureListener { exception ->
+                    // wyświetlenie komunikatu o błędzie podczas dodawania markera
+                    Toast.makeText(
+                        this,
+                        "Błąd podczas dodawania markera: ${exception.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+
+        }
     }
 
     override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
